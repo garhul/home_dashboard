e77bedf1a12e23bf78b1c7d5c02724b11b6fd2ce
Adrian Gesto <adrian.gesto@gmail.com>
2021-11-02 16:29:30 +0100

    updated env vars

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Starting up server"
-docker run -v "$(pwd)":/data -d -p 3030:3030 dash_sv
+docker run -v "$(pwd)":/data -d -p 3030:3030 \
+  -e \ SCAN_ADDR_BASE="192.168.0."
+  -e \ MQTT_BROKER="mqtt://192.168.0.135"
+  -e \ 
+  dash_sv
 
 echo "Starting up client"
 docker run -d -p 3000:5000 dash_web
