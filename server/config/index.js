aea4a0dbd832ed894f2760064ae0fd38df99452c
Adrian Gesto <adrian.gesto@gmail.com>
2021-11-02 16:19:02 +0100

    add env vars

--- server/config/index.js
+++ server/config/index.js
@@ -1,23 +1,23 @@
-const { resolve } = require('path');
+const path = require('path');
 
 const baseCfg = {
   scanAtStartup: false, //true,
   mockDevices: true,
   mockSensors: true,
   wsPort: process.env.WS_PORT || 3030,
-  baseScanAddress: '192.168.1.',
+  baseScanAddress: process.env.SCAN_ADDR_BASE || '192.168.1.',
   scanBatchSize: 24,
   scanTimeout: 5000,
   logger: {
-    path: null,
+    path: process.env.LOG_FILE  || null,
   },
   mqtt: {
-    broker: 'mqtt://192.168.1.10',
-    announceTopic: 'announce',
-    sensorsTopic: 'sensors',
+    broker: process.env.MQTT_BROKER || 'mqtt://192.168.1.10',
+    announceTopic: process.env.MQTT_ANNOUNCE_T ||'announce',
+    sensorsTopic: process.env.MQTT_SENSORS_T || 'sensors',
   },
   sensors: {
-    dataPath: resolve(__dirname, '../data'),
+    dataPath: path.resolve(__dirname, '../data'),
     persistToFile: false,
     recoverOnStartup: true,
 
@@ -36,17 +36,16 @@ const cfg = {
     mockDevices: false,
     mockSensors: false,
     logger: {
-      path: resolve('/data/dashboard_sv.log')
+      path: process.env.LOG_FILE || path.resolve('/data/dashboard_sv.log')
     },
     sensors: {
-      dataPath: resolve('/data'),
-      persistToFile: true,
-      recoverOnStartup: true,
-      timeSeriesDepth: 288,
+      dataPath: process.env.SENSOR_DATA_PATH || path.resolve('/data'),
+      persistToFile: process.env.SENSOR_PERSIST || true,
+      recoverOnStartup: process.env.SENSOR_RECOVER || true,
+      timeSeriesDepth: process.env.SENSOR_DEPTH || 288,
     },
   },
   dev: {}
 }
-
 
 module.exports = Object.assign({}, baseCfg, cfg[process.env.NODE_ENV]);
