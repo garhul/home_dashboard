7dcdc3c8cb08cdd98a4fb32113b2fb722ffef75e
Adrian Gesto <adrian.gesto@gmail.com>
2021-11-04 18:25:36 +0100

    fix startup

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting up server"
-docker run -v "$(pwd)":/data --name "dashboard_sv" -d -p 3030:3030 \
+docker run -v "$(pwd)/data":/data --name "dashboard_sv" -d -p 3030:3030 \
   -e SCAN_ADDR_BASE="192.168.0." \
   -e LOG_PATH="/data/dashboard_log.txt" \
   -e MQTT_BROKER="mqtt://127.0.0.1" dash_sv
