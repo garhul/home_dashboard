a9b6f26cbd7bd462fcfd8b24d0c35dea95dcfc12
Adrian Gesto <adrian.gesto@gmail.com>
2021-11-04 17:57:08 +0100

    add container names on run

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Starting up server"
-docker run -v "$(pwd)":/data -d -p 3030:3030 \
+docker run -v "$(pwd)":/data --name "dashboard_sv" -d -p 3030:3030 \
   -e \ SCAN_ADDR_BASE="192.168.0."
   -e \ MQTT_BROKER="mqtt://192.168.0.135"
   -e \ 
   dash_sv
 
 echo "Starting up client"
-docker run -d -p 3000:5000 dash_web
+docker run --name "dashboard_client" -d -p 3000:5000 dash_web
